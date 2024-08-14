780b3d9a6f2060008cd5e235149d08c4a46a3768
AshJPeek <a.j.peek97@gmail.com>
2024-08-14 15:17:26 +0100

    Implemented basic data caching on get endpoints.

--- ApiProtection/Controllers/UsersController.cs
+++ ApiProtection/Controllers/UsersController.cs
@@ -8,15 +8,17 @@ namespace ApiProtection.Controllers
     public class UsersController : ControllerBase
     {
         [HttpGet]
+        [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Any, NoStore = false)]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { Random.Shared.Next(1, 101).ToString() };
         }
 
         [HttpGet("{id}")]
+        [ResponseCache(Duration = 20, Location = ResponseCacheLocation.Any, NoStore = false)]
         public string Get(int id)
         {
-            return "value";
+            return $"Random Number: { Random.Shared.Next(1, 101) } for Id {id}";
         }
 
         [HttpPost]
